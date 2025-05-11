e1c65c15ec33b7c9b9500ef0c8d5d912db9bd400
Midhun Prahash SR <midhun.prahash@excelschools.edu.in>
2025-05-12 01:22:49 +0530

    feat: add arithmetic operations example with numpy

---
+++ Data Science/NumPy/Examples/# Arithmetic operations.py
@@ -0,0 +1,10 @@
+
+# Arithmetic operations
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Element-wise multiplication
+print(a / b)  # Division
+print(a ** 2) # Power operation
